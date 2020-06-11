10c382962a0fea97505b7f2de0ec358188832a2e
Woosik Koong <woosik.koong@gmail.com>
2020-06-11 16:13:04 -0700

    Add mdx page by configuring gatsby-plugin-mdx

--- gatsby-config.js
+++ gatsby-config.js
@@ -2,5 +2,16 @@ module.exports = {
   siteMetadata: {
     title: "BMOTIVATE",
   },
-  plugins: ["gatsby-plugin-emotion", "gatsby-plugin-react-helmet"],
+  plugins: [
+    "gatsby-plugin-emotion",
+    "gatsby-plugin-react-helmet",
+    {
+      resolve: "gatsby-plugin-mdx",
+      options: {
+        defaultLayouts: {
+          default: require.resolve("./src/components/layout"),
+        },
+      },
+    },
+  ],
 };
